ebccccbb76e242f1183ca4573672cd3ffa35ce30
Jubril <jubakare@gmail.com>
2022-06-04 18:07:54 +0100

    Changing sizes on phone screen

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def webhook():
         # NOTIFY BRIDGET OF THE NEW BOOKING
         newb_params = {
             'api_token': bsms_token,
-            'to': '2348143466411',
+            'to': '2348090659859',
             'from': 'New Booking',
             'body': f'New {event_name} with {name} at {event_time} on {event_date}. Visit www.lekkiajahikoyi.com/admin for more details. ',
         }
